110893ee027f39b59e0bc7fca74f16160d854414
Stefan <Stefan@129.21.149.64>
2013-12-04 15:18:19 -0500

    Added functionality in driver class, and put the proper import and receive method in line and jail

--- src/Line.scala
+++ src/Line.scala
@@ -1,9 +1,16 @@
+import akka.actor.Actor
+import akka.actor.ActorRef
+
 /**
  * Line holds a queue, security station, and scanners(line creates these objects)
  * A passenger is assigned to a line
  * 
  **/
 
-class Line(){
+class Line() extends Actor{
 
+  def receive = {
+    case passenger : sendPassenger =>{}
+    
+  }
 }
