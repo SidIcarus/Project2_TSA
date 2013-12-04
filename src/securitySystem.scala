00c6633593fc3b91e5d5d3d52d7cb87a286d31bf
Stefan <Stefan@129.21.149.64>
2013-12-03 21:40:49 -0500

    Added sendPassenger class.
    Added some functionality to securitySystem

--- src/securitySystem.scala
+++ src/securitySystem.scala
@@ -1,8 +1,42 @@
+//import akka.actor.Actor;
+//import akka.actor.ActorRef;
+
 /**
  * The security system checks the documents of all the passengers and inserts the passenger
  * in a line
  **/
 
-class securitySystem(){
-
+class securitySystem() /*extends Actor*/{
+  
+  /**
+   * Gets a passenger to have the document checked for
+   */
+  
+  /*
+  def getPassenger = {
+    case passenger : sendPassenger => {
+      
+      if (checkDocument()){
+        //send the passenger to a line
+      }
+      else{
+        println("Passenger does not have the proper documenation and is turned away")
+      }
+    }
+  }
+*/
+  /**
+   * Checks the passengers documents. Has a 20% chance of failing
+   * 
+   * Returns true or false depending on whether it passed or not
+   */
+  def checkDocument() : Boolean = {
+    
+    val n = new scala.util.Random
+    
+    if (n.nextDouble() <= 0.2) 
+    	false
+    else true
+    
+  }
 }
