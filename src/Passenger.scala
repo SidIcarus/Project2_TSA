99463fa2f53b07d12b69be3a6c1eea0d91a309df
SidIcarus <app6141@gmail.com>
2013-12-04 22:58:56 -0500

    pushed

--- src/Passenger.scala
+++ src/Passenger.scala
@@ -3,11 +3,6 @@ package src
 /**
  * Passenger is the actor traveling through the security system, and is checked
  * for correct documents, and that the scanner picked nothing up
- *
  * @param baggage, a Baggage item attached to a each particular passenger 
  **/
-
-class Passenger(val baggage : Baggage, val num : Int){
- 
-
-}
+class Passenger(val baggage : Baggage, val num : Int){ }
--- src/securityStation.scala
+++ src/securityStation.scala
@@ -22,7 +22,7 @@ class securityStation(val jail : ActorRef) extends Actor {
   	     jail ! new sendPassenger(report.passenger)
   	     println("Passenger " + report.passenger.num + " has failed the scan inspection, and is being sent to jail.\n")
   	  }else
-  	    println("Passenger goes to heaven")
+  	    println("Passenger " + report.passenger.num + " goes to heaven")
   	}
   }
 }
